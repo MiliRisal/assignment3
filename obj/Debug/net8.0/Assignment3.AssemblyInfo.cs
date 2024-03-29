3cf5454508e8e4e2b692df33d0d00b68b12e97dc
MiliRisal <milirisal11@gmail.com>
2024-03-29 13:50:41 -0400

    My final commit

--- obj/Debug/net8.0/Assignment3.AssemblyInfo.cs
+++ obj/Debug/net8.0/Assignment3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6b0db46b5e5985a70180170f2511a6e10ab3af0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70df26dc3666dc51b291407afc9d4759c7befc47")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
